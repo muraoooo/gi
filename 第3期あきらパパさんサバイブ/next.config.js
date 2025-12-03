a068142dc5a38c193bad6ff798fcf2125688e487
村岡大地 <muraooo0302@gmail.com>
2025-12-03 12:02:28 +0900

    fix: Next.js設定にMUIのトランスパイル設定を追加

--- 第3期あきらパパさんサバイブ/next.config.js
+++ 第3期あきらパパさんサバイブ/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@mui/material', '@mui/system', '@mui/utils', '@mui/icons-material'],
+  experimental: {
+    esmExternals: 'loose',
+  },
 }
 
 module.exports = nextConfig
