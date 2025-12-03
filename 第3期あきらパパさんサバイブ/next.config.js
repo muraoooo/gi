705f3f6a535947a122931b0da548fc7f996f2b6b
村岡大地 <muraooo0302@gmail.com>
2025-12-03 12:02:38 +0900

    fix: Next.js設定を改善（webpack設定を追加）

--- 第3期あきらパパさんサバイブ/next.config.js
+++ 第3期あきらパパさんサバイブ/next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@mui/material', '@mui/system', '@mui/utils', '@mui/icons-material'],
-  experimental: {
-    esmExternals: 'loose',
+  transpilePackages: [
+    '@mui/material',
+    '@mui/system',
+    '@mui/utils',
+    '@mui/icons-material',
+    '@emotion/react',
+    '@emotion/styled',
+  ],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    return config;
   },
 }
 
